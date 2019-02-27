e8871674c65c276167ff7db9008619201f6467bf
Graham Swan <thinkswan@gmail.com>
2019-02-27 21:16:40 +0100

    Add nodemon and start and dev scripts

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>')
+})
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
