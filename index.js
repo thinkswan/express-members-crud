46ec65039fac9f8bd54b0a4baed0dd042bb403f2
Graham Swan <thinkswan@gmail.com>
2019-02-27 21:35:11 +0100

    Handle missing members errors

--- index.js
+++ index.js
@@ -11,7 +11,17 @@ const app = express()
 
 app.get('/api/members', (req, res) => res.json(members))
 app.get('/api/members/:id', (req, res) => {
-  res.json(members.filter(member => member.id === parseInt(req.params.id)))
+  const found = members.some(member => member.id === parseInt(req.params.id))
+
+  if (!found) {
+    return res
+      .status(404)
+      .json({ msg: `Could not find member with id ${req.params.id}` })
+  }
+
+  return res.json(
+    members.filter(member => member.id === parseInt(req.params.id))
+  )
 })
 
 app.use(express.static(path.join(__dirname, 'public')))
