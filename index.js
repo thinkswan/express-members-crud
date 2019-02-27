68958c2cdbd31bd11838dc064bd7ec6b10f088d3
Graham Swan <thinkswan@gmail.com>
2019-02-27 21:33:45 +0100

    Add GET /api/members/:id route

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const path = require('path')
 const express = require('express')
-const logger = require('./middleware/logger')
+// const logger = require('./middleware/logger')
 const members = require('./Members')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.use(logger)
+// app.use(logger)
 
 app.get('/api/members', (req, res) => res.json(members))
+app.get('/api/members/:id', (req, res) => {
+  res.json(members.filter(member => member.id === parseInt(req.params.id)))
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 
