e68047f19dc06bb769f87020ed00a05e360f3743
Graham Swan <thinkswan@gmail.com>
2019-02-27 21:54:42 +0100

    Add PUT /api/members/:id route

--- routes/api/members.js
+++ routes/api/members.js
@@ -16,29 +16,52 @@ router.get('/:id', (req, res) => {
       .json({ msg: `Could not find member with id ${req.params.id}` })
   }
 
-  return res.json(
-    members.filter(member => member.id === parseInt(req.params.id))
-  )
+  const member = members.filter(
+    member => member.id === parseInt(req.params.id)
+  )[0]
+
+  return res.json(member)
 })
 
 // Create member
 router.post('/', (req, res) => {
-  const newMember = {
-    id: uuid.v4(),
-    name: req.body.name,
-    email: req.body.email,
-    status: 'active'
-  }
-
-  if (!newMember.name || !newMember.email) {
+  if (!req.body.name || !req.body.email) {
     return res
       .status(400)
       .json({ msg: 'Missing required name and email params' })
   }
 
-  members.push(newMember)
+  members.push({
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+    status: 'active'
+  })
 
   return res.json(members)
+})
+
+// Update member
+router.put('/:id', (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id))
+
+  if (!found) {
+    return res
+      .status(404)
+      .json({ msg: `Could not find member with id ${req.params.id}` })
+  }
+
+  const member = members.filter(
+    member => member.id === parseInt(req.params.id)
+  )[0]
+
+  if (req.body.name) member.name = req.body.name
+  if (req.body.email) member.email = req.body.email
+
+  return res.json({
+    msg: `Successfully updated member with id ${req.params.id}`,
+    member
+  })
 })
 
 module.exports = router
