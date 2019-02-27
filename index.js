bfe59dfd6fc822c932191e2c1f3e998af21e399d
Graham Swan <thinkswan@gmail.com>
2019-02-27 21:28:21 +0100

    Add logger middleware

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
+const logger = (req, res, next) => {
+  console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`)
+  next()
+}
+
+app.use(logger)
+
 app.get('/api/members', (req, res) => res.json(members))
 
 app.use(express.static(path.join(__dirname, 'public')))
