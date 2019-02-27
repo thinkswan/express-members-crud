f52285a87552292f7fbb66a085bb083ae8bd052d
Graham Swan <thinkswan@gmail.com>
2019-02-27 21:21:27 +0100

    Set public/ as static folder

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
