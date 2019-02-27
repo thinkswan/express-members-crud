557f3fffc5a0efae69f9ce764a3c5d01b12a5c8e
Graham Swan <thinkswan@gmail.com>
2019-02-27 21:26:23 +0100

    Add GET /api/members route

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const path = require('path')
 const express = require('express')
+const members = require('./Members')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+app.get('/api/members', (req, res) => res.json(members))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
