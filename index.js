725d40a6b2c5e277a91302574c9d31192f234cc8
Graham Swan <thinkswan@gmail.com>
2019-02-27 22:03:43 +0100

    Add express-handlebars and create layout and view

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const exphbs = require('express-handlebars')
 // const logger = require('./middleware/logger')
 
 const PORT = process.env.PORT || 5000
@@ -8,11 +9,21 @@ const app = express()
 
 // app.use(logger)
 
+// Handlebars middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// Body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// Render Handlebars view
+app.get('/', (req, res) => res.render('index'))
+
+// Set `public/` as static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Use `/api/members` routes
 app.use('/api/members', require('./routes/api/members'))
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
